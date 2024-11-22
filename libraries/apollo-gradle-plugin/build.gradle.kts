6980cf48a4600b06042054d9e6bbf8f013e5e68d
Martin Bonnin <martin@mbonnin.net>
2024-11-22 15:09:23 +0100

    dump R8 version

--- libraries/apollo-gradle-plugin/build.gradle.kts
+++ libraries/apollo-gradle-plugin/build.gradle.kts
@@ -100,6 +100,7 @@ if (relocateJar) {
     inputs.file(rulesFile)
     mainClass.set("com.android.tools.r8.R8")
 
+    args("--version")
     args("--release")
     args("--classfile")
     args("--output")
